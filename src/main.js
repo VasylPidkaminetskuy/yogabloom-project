c67779e49da5fb3272757b7d045788abd6e60ae4
Vitalii Shumakov <fafde8120@gmail.com>
2025-05-21 00:30:28 +0200

    fix last

--- src/main.js
+++ src/main.js
@@ -1,22 +1,14 @@
-// (() => {
-//       const refs = {
-//         openModalBtn: document.querySelector("[data-menu-open]"),
-//         closeModalBtn: document.querySelector("[data-menu-close]"),
-//         modal: document.querySelector("[data-menu]"),
-//       };
+(() => {
+      const refs = {
+        openModalBtn: document.querySelector("[data-menu-open-main]"),
+        closeModalBtn: document.querySelector("[data-menu-close-main]"),
+        modal: document.querySelector("[data-menu-main]"),
+      };
 
-//       refs.openModalBtn.addEventListener("click", toggleModal);
-//       refs.closeModalBtn.addEventListener("click", toggleModal);
+      refs.openModalBtn.addEventListener("click", toggleModal);
+      refs.closeModalBtn.addEventListener("click", toggleModal);
 
-//       function toggleModal() {
-//         refs.modal.classList.toggle("is-open");
-//       }
-
-//       // Закрытие меню при переходе по якорной ссылке
-//       const menuLinks = refs.modal.querySelectorAll("a[href^='#']");
-//       menuLinks.forEach(link => {
-//         link.addEventListener("click", () => {
-//           refs.modal.classList.remove("is-open");
-//         });
-//       });
-//     })();
+      function toggleModal() {
+        refs.modal.classList.toggle("is-open");
+      }
+    })();
